c9e5eb7e4e7ed92303df0507572ce1b3b6785e73
toad <toad@gmail.com>
2015-11-26 01:49:27 +0100

    Remove flyspell in latex by default

--- settings/setup-latex.el
+++ settings/setup-latex.el
@@ -30,7 +30,7 @@
 
 ;; (add-hook 'LaTeX-mode-hook 'auto-fill-mode)
 (add-hook 'LaTeX-mode-hook 'visual-line-mode)
-(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+;; (add-hook 'LaTeX-mode-hook 'flyspell-mode)
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
