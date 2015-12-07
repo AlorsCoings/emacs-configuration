cdbb4ecccb398bc1d4f5ad84558a318b146c5551
toad <toad@gmail.com>
2015-12-07 01:30:32 +0100

    Set current theme as safe (update requirement)

--- emacs-custom.el
+++ emacs-custom.el
@@ -251,7 +251,7 @@
  '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
  '(custom-safe-themes
    (quote
-    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+    ("79a3f477ac0cb4a106f78b6109614e991564a5c2467c36e6e854d4bc1102e178" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(eww-download-directory "~/EWW_Downloads/")
  '(fci-rule-color "#073642")
  '(flycheck-c/c++-googlelint-executable "/usr/local/bin/cpplint.py")
@@ -273,7 +273,8 @@
      (enable-old exact "where-is")
      (enable prefix "xref-")
      (disable exact "todo-add-category")
-     (enable exact "find-tag"))))
+     (enable exact "find-tag")
+     (enable prefix "etags-select-"))))
  '(inhibit-startup-screen t)
  '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(ispell-highlight-face (quote custom-themed))
