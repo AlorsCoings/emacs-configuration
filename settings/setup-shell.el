209bade63632e273e92ba2ed8c395ad7245af1da
AlvaGingra <gros.nicolas0@gmail.com>
2017-02-21 13:59:39 +0100

    Add some paths to PATH variable and set JAVA_HOME variable

--- settings/setup-shell.el
+++ settings/setup-shell.el
@@ -22,6 +22,8 @@
 (add-hook 'shell-command-complete-functions
           'bash-completion-dynamic-complete)
 
+(setenv "PATH" "/home/toad/anaconda3/bin:/usr/lib/jvm/jdk1.8/bin:/usr/local/apache-maven-3.3.9/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usrr/games:/usr/local/games:/home/toad/local/bin:/usr/local/apache-log4j-1.2.15:/snap/bin:/snap/bin")
+(setenv "JAVA_HOME" "/usr/lib/jvm/jdk1.8")
 (setenv "SHELL" "/bin/bash")
 (require 'exec-path-from-shell)
 (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
