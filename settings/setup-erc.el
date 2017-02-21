5f98287d4e9807b9317f576eb07aeefb883e0128
AlvaGingra <gros.nicolas0@gmail.com>
2017-02-21 14:17:30 +0100

    Add erc package

---
+++ settings/setup-erc.el
@@ -0,0 +1,15 @@
+;;; package --- setup-erc
+
+;;; Commentary:
+;;; Emacs configuration file for erc
+
+;;; Code:
+
+(require 'erc)
+
+(load (concat user-emacs-directory ".erc-auth"))
+
+
+
+(provide 'setup-erc)
+;;; setup-erc.el ends here
