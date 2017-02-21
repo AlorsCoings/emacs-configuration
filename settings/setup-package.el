2698f81ec82625380ef6e10e84e5503618675215
AlvaGingra <gros.nicolas0@gmail.com>
2017-02-21 14:00:56 +0100

    Change melpa url for packages. Remove melpa-stable and add elpy.

--- settings/setup-package.el
+++ settings/setup-package.el
@@ -8,11 +8,10 @@
 (require 'package)
 
 
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("elpy" . "https://jorgenschaefer.github.io/packages/") t)
 
 ;; Initialize installed packages
 (package-initialize)
